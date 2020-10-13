39f55eeec1111a69909d4ef30aabf6c317597ba2
nicolas-kings <1443733886@qq.com>
2020-10-13 09:47:54 +0800

    功能更新

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
+# Scriptables ——网易云热评
+iOS14桌面组件神器（Scriptable）原创脚本，精美作品分享！    
 
-<section id="nice" data-tool="mdnice编辑器" data-website="https://www.mdnice.com" style="font-size: 16px; color: black; padding: 0 10px; line-height: 1.6; word-spacing: 0px; letter-spacing: 0px; word-break: break-word; word-wrap: break-word; text-align: left; font-family: Optima-Regular, Optima, PingFangSC-light, PingFangTC-light, 'PingFang SC', Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;"><h2 data-tool="mdnice编辑器" style="margin-top: 30px; margin-bottom: 15px; padding: 0px; font-weight: bold; color: black; border-bottom: 2px solid rgb(239, 112, 96); font-size: 1.3em;"><span class="prefix" style="display: none;"></span><span class="content" style="display: inline-block; font-weight: bold; background: rgb(239, 112, 96); color: #ffffff; padding: 3px 10px 1px; border-top-right-radius: 3px; border-top-left-radius: 3px; margin-right: 3px;"><center>Scriptable脚本——网易云音乐 </center></span><span class="suffix"></span><span style="display: inline-block; vertical-align: bottom; border-bottom: 36px solid #efebe9; border-right: 20px solid transparent;"> </span></h2>
-<figure data-tool="mdnice编辑器" style="margin: 0; margin-top: 10px; margin-bottom: 10px; display: flex; flex-direction: column; justify-content: center; align-items: center;"><img src="https://ae02.alicdn.com/kf/H1c0424df7ba54eeab67850dfe275134e0.png" alt="脚本预览" style="display: block; margin: 0 auto; max-width: 100%;"><figcaption style="margin-top: 5px; text-align: center; color: #888; font-size: 14px;">脚本预览</figcaption></figure>
-</section>
+![效果图](https://ae02.alicdn.com/kf/H1c0424df7ba54eeab67850dfe275134e0.png)
+
+# 如何使用
+1. iPhone 上下载 [Scriptable](https://apps.apple.com/cn/app/scriptable/id1405459188) App（确保你的系统已更新为 iOS14+）. 
+2. 打开App，点击右上角 + 号，复制项目中对应的代码  `hotcomments.js`， [点击下载](https://github.com/Nicolasking007/Scriptable/blob/main/hotcomments.js)
+3. 长按桌面，添加组件，选择 `Scriptable`，然后点击组件配置，选择刚刚保存的脚本即可.
+4. 尽情享受吧！   
+
+![流程图](https://ae04.alicdn.com/kf/H5365bd53284746528cac6cfcf3befcb0L.png)
+
+## 版本
+### `V0.1.1`   
+1、修正点击直链播放 改为跳转网易云播放.
+
+### ` V0.1.2 `   
+1、更换api接口，更稳定。接口来源于[优启梦 ](https://api.uomg.com/).感谢
+
+2、新增从相册中选用背景图片，这样你就可以使用透明背景了，具体怎么生成`透明壁纸 `这里我就不再赘述.
+
+<center>
+    <img src="https://ae04.alicdn.com/kf/H697021382f264fd2ad0476c7e817b309g.png" style="width: 100px;">
+</center>
+
+**更多好玩的脚本正在码字中...`尽情期待！`**
--- hotcomments.js
+++ hotcomments.js
@@ -2,46 +2,77 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: brown; icon-glyph: magic;
 // author:Nicolas-kings
-// ver:0.1.1
+// ver:0.1.2
 const res = await getData();
-// 初始化组件ui
-let widget = await createWidget(res)
-// const url = "https://area.sinaapp.com/bingImg/"   //使用必应壁纸作为背景
-// const url = res.music_pic   //使用歌曲封面作为背景
+const files = FileManager.local()
+const imageBackground = false  //对于自定义图片背景，设置为true；默认设置为false.
+const widgetPreview = "medium"
+const forceImageUpdate = true  
+//const url = "https://area.sinaapp.com/bingImg/"   //使用必应壁纸作为背景时，请注释下面
+//const url = res.data.picurl   //使用歌曲封面作为背景，，请注释上面
 const url = "http://p1.music.126.net/uarVFKgUlrI9Z1nr-50cAw==/109951162843608471.jpg"     //固定一张图片,这里我选用城南花已开的封面,图片不能太大，容易崩溃
 const i = await new Request(url);
 const img = await i.loadImage();
-widget.backgroundImage = img
 // 如果不是在组件执行，则显示预览
-if (!config.runsInWidget) {
-  await widget.presentMedium()
-}
+// if (!config.runsInWidget) {
+//   await widget.presentMedium()
+// }
 // 设置桌面组件
+let widget = await createWidget(res)
+if (imageBackground) {
+
+  const path = files.joinPath(files.documentsDirectory(), "hot-comments-image")
+  const exists = files.fileExists(path)
+
+  // 如果存在并且不强制进行更新，将使用缓存。
+  if (exists && (config.runsInWidget || !forceImageUpdate)) {
+    widget.backgroundImage = files.readImage(path)
+
+    // 如果在小部件中运行时缺少它，请使用灰色背景。
+  } else if (!exists && config.runsInWidget) {
+
+    widget.backgroundColor = Color.gray()
+    // 但是，如果我们在应用程序中运行，则提示用户输入图像。
+  } else {
+    const img = await Photos.fromLibrary()
+    widget.backgroundImage = img
+    files.writeImage(path, img)
+  }
+
+}
+else {
+  widget.backgroundImage = await shadowImage(img)
+}
+
 Script.setWidget(widget)
+if (widgetPreview == "small") { widget.presentSmall() }
+else if (widgetPreview == "medium") { widget.presentMedium() }
+else if (widgetPreview == "large") { widget.presentLarge() }
+
 Script.complete()
 
 // 创建组件
 async function createWidget() {
   let w = new ListWidget()
   w.backgroundColor = new Color("#222222", 1)
-  //   标题
+
+  //  标题
   let title = w.addText("\u7f51\u6613\u4e91\u70ed\u8bc4")
   title.font = Font.boldMonospacedSystemFont(20)
   title.textColor = Color.white()
   title.url = "orpheuswidget://"
-  w.addSpacer(8)
+  w.addSpacer(12)
 
-  //   内容
-  let body = w.addText(`❝${res.comments}❞ `)
+  //  内容
+  let body = w.addText(`❝${res.data.content}❞ `)
   body.font = Font.lightMonospacedSystemFont(18)
   body.textColor = Color.white()
   body.textOpacity = 0.88
   // body.url = res.music_url     //跳转直链播放
-  body.url = `orpheus://song/${res.music_url.split('?')[1].split('=')[1].split('.')[0]}`     //调整点击小组件 默认跳转网易云音乐进行播放
+  body.url = `orpheus://song/${res.data.url.split('?')[1].split('=')[1].split('.')[0]}`     //默认跳转网易云音乐进行播放
   w.addSpacer(8)
 
-
-  let foot = w.addText(`—— 评论来自歌曲「${res.name}」`);
+  let foot = w.addText(`—— 评论来自歌曲「${res.data.name}」`);
   foot.font = Font.lightMonospacedSystemFont(12)
   foot.textColor = Color.orange();
   foot.textOpacity = 0.88;
@@ -51,7 +82,7 @@ async function createWidget() {
 }
 
 async function getData() {
-  const url = 'https://api.66mz8.com/api/music.163.php'
+  const url = 'https://api.uomg.com/api/comments.163'
   const request = new Request(url)
   const res = await request.loadJSON()
   console.log(res)
@@ -59,3 +90,17 @@ async function getData() {
 
 }
 
+async function shadowImage(img) {
+  let ctx = new DrawContext()
+  // 把画布的尺寸设置成图片的尺寸
+  ctx.size = img.size
+  // 把图片绘制到画布中
+  ctx.drawImageInRect(img, new Rect(0, 0, img.size['width'], img.size['height']))
+  // 设置绘制的图层颜色，为半透明的黑色
+  ctx.setFillColor(new Color('#000000', 0.5))
+  // 绘制图层
+  ctx.fillRect(new Rect(0, 0, img.size['width'], img.size['height']))
+
+  // 导出最终图片
+  return await ctx.getImage()
+}
